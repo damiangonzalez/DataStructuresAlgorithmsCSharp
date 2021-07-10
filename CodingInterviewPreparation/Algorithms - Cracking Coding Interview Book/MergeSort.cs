aa2ecf6793b901739d699903ad2fc007a16a33bc
Damian Gonzalez <damian_gonzalez@yahoo.com>
2021-07-10 16:02:19 -0400

    Merge sort

--- CodingInterviewPreparation/Algorithms - Cracking Coding Interview Book/MergeSort.cs
+++ CodingInterviewPreparation/Algorithms - Cracking Coding Interview Book/MergeSort.cs
@@ -1,3 +1,6 @@
+using System;
+using NUnit.Framework;
+
 namespace Algorithms
 {
     // https://ankitsharmablogs.com/merge-sort-algorithm-in-c-sharp/
@@ -12,9 +15,60 @@ namespace Algorithms
             mergeSort(array, helper, 0, array.Length - 1);
         }
 
-        public void mergeSort(int[] array, int[] helper, int start, int end)
+        public void mergeSort(int[] array, int[] helper, int low, int high)
         {
-            
+            if (low < high)
+            {
+                int middle = (low + high) / 2;
+                mergeSort(array, helper, low, middle);
+                mergeSort(array, helper, middle + 1, high);
+                merge(array, helper, low, middle, high);
+            }
+        }
+
+        public void merge(int[] array, int[] helper, int low, int middle, int high)
+        {
+            for (int i = low; i <= high; i++)
+            {
+                helper[i] = array[i];
+            }
+
+            int helperLeft = low;
+            int helperRight = middle + 1;
+            int current = low;
+
+            while (helperLeft <= middle && helperRight <= high)
+            {
+                if (helper[helperLeft] <= helper[helperRight])
+                {
+                    array[current] = helper[helperLeft];
+                    helperLeft++;
+                }
+                else
+                {
+                    array[current] = helper[helperRight];
+                    helperRight++;
+                }
+
+                current++;
+            }
+
+            int remainingLeft = middle - helperLeft;
+            for (int i = 0; i <= remainingLeft; i++)
+            {
+                array[current + i] = helper[helperLeft + i];
+            }
+        }
+
+        [Test] // static void Main(string[] args)
+        public void MainTest()
+        {
+            int[] array = new[] {2, 1, 3};
+            mergeSort(array);
+            foreach (int i in array)
+            {
+                Console.WriteLine(i);
+            }
         }
     }
 }
