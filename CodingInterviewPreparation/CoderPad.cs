db45e8f9c376a20858b2739a23e07b48db2d0205
Damian Gonzalez <damian_gonzalez@yahoo.com>
2021-07-12 22:47:27 -0400

    Adding coderpad NUnit/NUnitLite sample with TestCase annotations

---
+++ CodingInterviewPreparation/CoderPad.cs
@@ -0,0 +1,26 @@
+using NUnit.Framework;
+using NUnitLite;
+using System;
+using System.Reflection;
+
+public class Runner {
+    public static int Main2(string[] args) {
+        return new AutoRun(Assembly.GetCallingAssembly()).Execute(new String[] {"--labels=All"});
+    }
+
+    [TestFixture]
+    public class Dog {
+        public String bark() {
+            return "bark";
+        }
+
+        [TestCase("bark")]
+        [TestCase("bow")]
+        [TestCase("sdf")]
+        [TestCase("hi")]
+        public void TestBarker(string input) {
+            Console.WriteLine(input);
+            Assert.AreEqual(input, new Dog().bark());
+        }
+    }
+}
